ed7d0e4bcbe4e6aea176c230c447b1bb7d57d588
GabHV <99384758+GabHV@users.noreply.github.com>
2022-06-01 12:36:16 +0200

    Archivo faltante del mapa del jefe final

---
+++ beta_version/assets/tilemaps/TronoNuevoCorrect.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TronoNuevoCorrect" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="Texture/TronoNuevoCorrect.png" width="110" height="198"/>
+</tileset>
